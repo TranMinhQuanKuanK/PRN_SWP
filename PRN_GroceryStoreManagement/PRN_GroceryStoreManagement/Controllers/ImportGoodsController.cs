b2e043f807c53a2bf58a1f2257ca6556f40dcda7
Trần Minh Quân <54695160+TranMinhQuanKun@users.noreply.github.com>
2021-06-22 21:47:09 +0700

    hm

--- PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/Controllers/ImportGoodsController.cs
+++ PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/Controllers/ImportGoodsController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PRN_GroceryStoreManagement.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ImportGoodsController : Controller
     {
         public IActionResult Index()
