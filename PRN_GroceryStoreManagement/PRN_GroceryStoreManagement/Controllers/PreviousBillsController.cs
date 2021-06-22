58c95d8a741bda4a54bb6c2befe068c25f593da9
Trần Minh Quân <54695160+TranMinhQuanKun@users.noreply.github.com>
2021-06-22 21:43:56 +0700

    hmmm

--- PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/Controllers/PreviousBillsController.cs
+++ PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/Controllers/PreviousBillsController.cs
@@ -10,7 +10,7 @@ namespace PRN_GroceryStoreManagement.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("PreviousBills");
         }
     }
 }
