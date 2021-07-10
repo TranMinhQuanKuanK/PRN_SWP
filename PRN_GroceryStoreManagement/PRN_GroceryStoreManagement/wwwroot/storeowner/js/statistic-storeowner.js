bbf0f413481ad4e5b6351b26e15dd215a98b6e45
Trần Minh Quân <54695160+TranMinhQuanKun@users.noreply.github.com>
2021-07-10 21:01:27 +0700

    update cua Quan

--- PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/wwwroot/storeowner/js/statistic-storeowner.js
+++ PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/wwwroot/storeowner/js/statistic-storeowner.js
@@ -2,9 +2,9 @@ function showProductStatistic() {
     var request = new XMLHttpRequest();
     
     var url = "GetProductStatistic";
-    url += "?date-from=" + document.getElementById("date-from").value;
-    url += "&date-to=" + document.getElementById("date-to").value;
-    url += "&sort-by=" + document.getElementById("sort-by").value;
+    url += "?date_from=" + document.getElementById("date-from").value;
+    url += "&date_to=" + document.getElementById("date-to").value;
+    url += "&sort_by=" + document.getElementById("sort-by").value;
     
     request.open('GET', url, true);
     request.onload = function () {
