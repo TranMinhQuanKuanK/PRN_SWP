794d77cc08d23dd7d4985a07f4bf73ff3438665c
Trần Minh Quân <54695160+TranMinhQuanKun@users.noreply.github.com>
2021-07-15 23:02:31 +0700

    fix cashier

---
+++ PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/APIControllers/Cashier/ClearBill_APIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PRN_GroceryStoreManagement.Models.sessionBill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRN_GroceryStoreManagement.APIControllers.Cashier
+{
+    [Authorize(Roles = "Cashier")]
+    [Route("ClearBill")]
+    [ApiController]
+    public class ClearBill_APIController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            HttpContext.Session.SetString("BILL", JsonConvert.SerializeObject(new BillObj()));
+            return new JsonResult(null);
+        }
+    }
+}
---
+++ PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/APIControllers/Cashier/SendFeedback_APIController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRN_GroceryStoreManagement.Models.feedback;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PRN_GroceryStoreManagement.APIControllers.Cashier
+{
+    [Authorize(Roles = "Cashier")]
+    [Route("SendFeedback")]
+    [ApiController]
+    public class SendFeedback_APIController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendFeedback([FromBody] JsonElement JsonObj)
+        {
+            String feedback_content = JsonObj.GetProperty("feedback_content").GetString();
+            Debug.WriteLine("Content: " + feedback_content);
+            if (feedback_content.Length <= 1000)
+            {
+                String cashier_username =  HttpContext.Session.GetString("USERNAME");
+                FeedbackDAO fDAO = new FeedbackDAO();
+                fDAO.createFeedback(DateTime.Now,
+                        feedback_content, cashier_username);
+            }
+            return new JsonResult(null);
+        }
+    }
+}
