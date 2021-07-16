1d6a0cf077bc4e3c52e8217dbbdc5dea073e735f
Trần Minh Quân <54695160+TranMinhQuanKun@users.noreply.github.com>
2021-07-16 23:32:26 +0700

    try catch ??

--- PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/APIControllers/Cashier/RemoveProductFromBill_APIController.cs
+++ PRN_GroceryStoreManagement/PRN_GroceryStoreManagement/APIControllers/Cashier/RemoveProductFromBill_APIController.cs
@@ -19,49 +19,58 @@ namespace PRN_GroceryStoreManagement.APIControllers.Common
     [ApiController]
     public class RemoveProductFromBill_APIController : ControllerBase
     {
-        
+
         [HttpGet]
         public IActionResult RemoveProductFromBill(int product_id)
         {
             //get bill from session
             BillObj bill = null;
             String BillJSONString = HttpContext.Session.GetString("BILL");
-            if (BillJSONString == null)
+            try
             {
-                bill = new BillObj();
-            }
-            else
-            {
-                bill = JsonConvert.DeserializeObject<BillObj>(BillJSONString);
-                bill.err_obj = new BillErrObj();
-            }
-
-            List<BillItemObject> details = bill.Bill_Detail;
-
-            //lấy vị trí
-            int result = -1;
-            for (int i = 0; i < details.Count; i++)
-            {
-                if (details[i].product.product_ID == product_id)
+            
+                if (BillJSONString == null)
                 {
-                    result = i;
+                    bill = new BillObj();
                 }
+                else
+                {
+                    bill = JsonConvert.DeserializeObject<BillObj>(BillJSONString);
+                    bill.err_obj = new BillErrObj();
+                }
+
+                List<BillItemObject> details = bill.Bill_Detail;
+
+                //lấy vị trí
+                int result = -1;
+                for (int i = 0; i < details.Count; i++)
+                {
+                    if (details[i].product.product_ID == product_id)
+                    {
+                        result = i;
+                    }
+                }
+                //xóa
+                if (result >= 0)
+                {
+                    BillItemObject selected_for_remove_Product = details[result];
+                    int price_lost = selected_for_remove_Product.product.selling_price
+                            * selected_for_remove_Product.quantity;
+                    details.RemoveAt(result);
+                    bill.total_cost = bill.total_cost - price_lost;
+                    bill.Bill_Detail = details;
+                    //set lại lỗi
+                    bill.err_obj = new BillErrObj();
+
+                    HttpContext.Session.SetString("BILL", JsonConvert.SerializeObject(bill));
+                }
+
+                return new JsonResult(bill);
             }
-            //xóa
-            if (result >= 0)
+            catch (Exception ex)
             {
-                BillItemObject selected_for_remove_Product = details[result];
-                int price_lost = selected_for_remove_Product.product.selling_price
-                        * selected_for_remove_Product.quantity;
-                details.RemoveAt(result);
-                bill.total_cost =  bill.total_cost - price_lost;
-                bill.Bill_Detail = details;
-                //set lại lỗi
-                bill.err_obj = new BillErrObj();
-
-                HttpContext.Session.SetString("BILL", JsonConvert.SerializeObject(bill));
+                Console.WriteLine(ex.Message);
             }
-
             return new JsonResult(bill);
         }
     }
